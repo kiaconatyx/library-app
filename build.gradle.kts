6c2b18a7cc66c1efcb149f6aa79733f2234c9496
HP <20088426@mail.wit.ie>
2022-11-26 21:41:50 +0000

    Persistence - Update Dependancies

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,6 +18,8 @@ dependencies {
     implementation("io.github.microutils:kotlin-logging:3.0.4")
     // https://mvnrepository.com/artifact/org.slf4j/slf4j-simple
     testImplementation("org.slf4j:slf4j-simple:2.0.5")
+    implementation("com.thoughtworks.xstream:xstream:1.4.19")
+    implementation("org.codehaus.jettison:jettison:1.5.2")
 
 
 }
