5b1370c94fbf180d5a472cc9b3667796c7db31a5
HP <20088426@mail.wit.ie>
2022-11-26 19:37:33 +0000

    Functionality - New Functionality books by rating BookAPI

--- src/main/kotlin/controllers/BookAPI.kt
+++ src/main/kotlin/controllers/BookAPI.kt
@@ -81,4 +81,34 @@ class BookAPI {
         }
         return counter
     }
+
+    fun listBooksBySelectedRating(rating: Int): String {
+        return if (books.isEmpty()) {
+            "No books stored"
+        } else {
+            var listOfBooks = ""
+            for (i in books.indices) {
+                if (books[i].bookRating == rating) {
+                    listOfBooks +=
+                        """$i: ${books[i]}
+                        """.trimIndent()
+                }
+            }
+            if (listOfBooks.equals("")) {
+                "No books with rating of: $rating"
+            } else {
+                "${numberOfBooksByRating(rating)} books with rating of $rating: $listOfBooks"
+            }
+        }
+    }
+
+    fun numberOfBooksByRating(rating: Int): Int {
+        var counter = 0
+        for (book in books) {
+            if (book.bookRating == rating) {
+                counter++
+            }
+        }
+        return counter
+    }
 }
