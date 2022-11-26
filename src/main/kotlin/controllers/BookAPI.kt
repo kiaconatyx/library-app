9cb84549bbab2107c5cc52d838bcfc5baa23be48
HP <20088426@mail.wit.ie>
2022-11-26 17:09:02 +0000

    Controller - BookAPI created

--- src/main/kotlin/controllers/BookAPI.kt
+++ src/main/kotlin/controllers/BookAPI.kt
@@ -1,4 +1,22 @@
 package controllers
 
+import models.Book
+
 class BookAPI {
+    private var books = ArrayList<Book>()
+
+    fun add(book: Book): Boolean {
+        return books.add(book)
+    }
+    fun listAllBooks(): String {
+        return if (books.isEmpty()) {
+            "No books stored"
+        } else {
+            var listOfBooks = ""
+            for (i in books.indices) {
+                listOfBooks += "${i}: ${books[i]} \n"
+            }
+            listOfBooks
+        }
+    }
 }
