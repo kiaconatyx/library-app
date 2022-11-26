ab865ccbf278552bc3080dfa95b624526d611a49
HP <20088426@mail.wit.ie>
2022-11-26 19:39:29 +0000

    Functionality - New Functionality books by ISBN BookAPI

--- src/main/kotlin/controllers/BookAPI.kt
+++ src/main/kotlin/controllers/BookAPI.kt
@@ -111,4 +111,34 @@ class BookAPI {
         }
         return counter
     }
+
+    fun listBooksBySelectedISBN(ISBN: Int): String {
+        return if (books.isEmpty()) {
+            "No books stored"
+        } else {
+            var listOfBooks = ""
+            for (i in books.indices) {
+                if (books[i].bookISBN == ISBN) {
+                    listOfBooks +=
+                        """$i: ${books[i]}
+                        """.trimIndent()
+                }
+            }
+            if (listOfBooks.equals("")) {
+                "No books with ISBN of: $ISBN"
+            } else {
+                "${numberOfBooksByISBN(ISBN)} books with ISBN of $ISBN: $listOfBooks"
+            }
+        }
+    }
+
+    fun numberOfBooksByISBN(ISBN: Int): Int {
+        var counter = 0
+        for (book in books) {
+            if (book.bookISBN == ISBN) {
+                counter++
+            }
+        }
+        return counter
+    }
 }
