7166574678e1918192950d73e9d60a7dd83569ed
HP <20088426@mail.wit.ie>
2022-11-26 17:13:49 +0000

    Controller - Include controller in Main.kt & refactor addBook & listBooks functions

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,12 @@
+import controllers.BookAPI
+import models.Book
 import mu.KotlinLogging
 import utils.ScannerInput
+import utils.ScannerInput.readNextInt
+import utils.ScannerInput.readNextLine
 import java.lang.System.exit
 private val logger = KotlinLogging.logger {}
+private val bookAPI = BookAPI()
 
 fun main(args: Array<String>) {
     runMenu()
@@ -39,11 +44,21 @@ fun runMenu() {
 }
 
 fun addBook(){
-    logger.info { "addBook() function invoked" }
+    val bookTitle = readNextLine("Enter the book's title: ")
+    val bookRating = readNextInt("Enter a rating for the book (1-low, 2, 3, 4, 5-high): ")
+    val bookISBN = readNextInt("Enter a unique ISBN for the book ")
+    val bookGenre = readNextLine("Enter a suitable genre for the book ")
+    val isAdded = bookAPI.add(Book(bookTitle, bookRating, bookISBN,bookGenre, false))
+
+    if (isAdded) {
+        println("Added Successfully")
+    } else {
+        println("Add Failed")
+    }
 }
 
 fun listBooks(){
-    logger.info { "listBooks() function invoked" }
+    println(bookAPI.listAllBooks())
 }
 
 fun updateBook(){
