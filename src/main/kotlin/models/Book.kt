4a174c99de636a110b24e061ab3c56ae40fb05d0
HP <20088426@mail.wit.ie>
2022-11-26 16:59:47 +0000

    Model - Adding model class called Book

---
+++ src/main/kotlin/models/Book.kt
@@ -0,0 +1,8 @@
+package models
+
+data class Book(val bookTitle: String,
+                val bookRating: Int,
+                val bookISBN: Int,
+                val bookGenre: String,
+                val isNoteArchived :Boolean){
+}
