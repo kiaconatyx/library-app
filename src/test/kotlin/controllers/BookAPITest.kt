11e2dd97268981c20bb77b0ee7a2c6a42c3b0b76
HP <20088426@mail.wit.ie>
2022-11-26 18:29:57 +0000

    Testing - Nesting Add & List in BookAPITest

--- src/test/kotlin/controllers/BookAPITest.kt
+++ src/test/kotlin/controllers/BookAPITest.kt
@@ -4,13 +4,14 @@ import models.Book
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 
 class BookAPITest {
 
     private var music101: Book? = null
-    private var finalfantasy : Book? = null
+    private var finalfantasy: Book? = null
     private var learntocode: Book? = null
     private var cookingfordummies: Book? = null
     private var dolphinworld: Book? = null
@@ -18,8 +19,8 @@ class BookAPITest {
     private var emptyBooks: BookAPI? = BookAPI()
 
     @BeforeEach
-    fun setup(){
-        music101 = Book("Music 101", 2, 100, "music",false)
+    fun setup() {
+        music101 = Book("Music 101", 2, 100, "music", false)
         finalfantasy = Book("Final Fantasy - The World Beyond", 1, 101, "fantasy", false)
         learntocode = Book("Learn to Code", 4, 102, "Tech", false)
         cookingfordummies = Book("Cooking for Dummies", 5, 103, "cooking", false)
@@ -33,7 +34,7 @@ class BookAPITest {
     }
 
     @AfterEach
-    fun tearDown(){
+    fun tearDown() {
         music101 = null
         finalfantasy = null
         learntocode = null
@@ -43,38 +44,44 @@ class BookAPITest {
         emptyBooks = null
     }
 
-    @Test
-    fun `adding a Book to a populated list adds to ArrayList`(){
-        val newBook = Book("Buddy Holly Life Story", 5, 107, "bio",false)
-        assertEquals(5, populatedBooks!!.numberOfBooks())
-        assertTrue(populatedBooks!!.add(newBook))
-        assertEquals(6, populatedBooks!!.numberOfBooks())
-        assertEquals(newBook, populatedBooks!!.findBook(populatedBooks!!.numberOfBooks() - 1))
+    @Nested
+    inner class AddBooks {
+        @Test
+        fun `adding a Book to a populated list adds to ArrayList`() {
+            val newBook = Book("Buddy Holly Life Story", 5, 107, "bio", false)
+            assertEquals(5, populatedBooks!!.numberOfBooks())
+            assertTrue(populatedBooks!!.add(newBook))
+            assertEquals(6, populatedBooks!!.numberOfBooks())
+            assertEquals(newBook, populatedBooks!!.findBook(populatedBooks!!.numberOfBooks() - 1))
+        }
+
+        @Test
+        fun `adding a Book to an empty list adds to ArrayList`() {
+            val newBook = Book("Buddy Holly Life Story", 5, 107, "bio", false)
+            assertEquals(0, emptyBooks!!.numberOfBooks())
+            assertTrue(emptyBooks!!.add(newBook))
+            assertEquals(1, emptyBooks!!.numberOfBooks())
+            assertEquals(newBook, emptyBooks!!.findBook(emptyBooks!!.numberOfBooks() - 1))
+        }
     }
 
-    @Test
-    fun `adding a Book to an empty list adds to ArrayList`(){
-        val newBook = Book("Buddy Holly Life Story", 5, 107, "bio", false)
-        assertEquals(0, emptyBooks!!.numberOfBooks())
-        assertTrue(emptyBooks!!.add(newBook))
-        assertEquals(1, emptyBooks!!.numberOfBooks())
-        assertEquals(newBook, emptyBooks!!.findBook(emptyBooks!!.numberOfBooks() - 1))
-    }
+    @Nested
+    inner class ListBooks {
+        @Test
+        fun `listAllBooks returns No Books Stored message when ArrayList is empty`() {
+            assertEquals(0, emptyBooks!!.numberOfBooks())
+            assertTrue(emptyBooks!!.listAllBooks().lowercase().contains("no books to see here"))
+        }
 
-    @Test
-    fun `listAllBooks returns No Books Stored message when ArrayList is empty`() {
-        assertEquals(0, emptyBooks!!.numberOfBooks())
-        assertTrue(emptyBooks!!.listAllBooks().lowercase().contains("no books to see here"))
-    }
-
-    @Test
-    fun `listAllBooks returns Books when ArrayList has books stored`() {
-        assertEquals(5, populatedBooks!!.numberOfBooks())
-        val booksString = populatedBooks!!.listAllBooks().lowercase()
-        assertTrue(booksString.contains("music 101"))
-        assertTrue(booksString.contains("learn to code"))
-        assertTrue(booksString.contains("cooking for dummies"))
-        assertTrue(booksString.contains("final fantasy - the world beyond"))
-        assertTrue(booksString.contains("dolphin world"))
+        @Test
+        fun `listAllBooks returns Books when ArrayList has books stored`() {
+            assertEquals(5, populatedBooks!!.numberOfBooks())
+            val booksString = populatedBooks!!.listAllBooks().lowercase()
+            assertTrue(booksString.contains("music 101"))
+            assertTrue(booksString.contains("learn to code"))
+            assertTrue(booksString.contains("cooking for dummies"))
+            assertTrue(booksString.contains("final fantasy - the world beyond"))
+            assertTrue(booksString.contains("dolphin world"))
+        }
     }
 }
