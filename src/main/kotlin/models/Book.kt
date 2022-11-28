1d2cb5c3f366cf8d9e96d32cf0ef950947e692f0
HP <20088426@mail.wit.ie>
2022-11-28 19:20:00 +0000

    Functionality - add library classes to Book.kt

--- src/main/kotlin/models/Book.kt
+++ src/main/kotlin/models/Book.kt
@@ -6,4 +6,57 @@ data class Book(var bookTitle: String,
                 var bookGenre: String,
                 var isBookArchived :Boolean = false,
                 var libraries : MutableSet<Library> = mutableSetOf()) {
+
+    private var lastLibraryId = 0
+    private fun getLibraryId() = lastLibraryId++
+
+    fun addLibrary(library: Library) : Boolean {
+        library.libraryId = getLibraryId()
+        return libraries.add(library)
+
+    }
+    fun numberOfLibraries() = libraries.size
+
+    fun findOne(id: Int): Library?{
+        return libraries.find{ library -> library.libraryId == id }
+    }
+
+    fun delete(id: Int): Boolean {
+        return books.removeIf { book -> book.bookId == id}
+    }
+
+    fun update(id: Int, newBook : Book): Boolean {
+        val foundBook = findOne(id)
+
+        //if the object exists, use the details passed in the newBook parameter to
+        //update the found object in the Set
+        if (foundBook != null){
+            foundBook.bookContents = newBook.bookContents
+            foundBook.isBookComplete = newBook.isBookComplete
+            return true
+        }
+
+        //if the object was not found, return false, indicating that the update was not successful
+        return false
+    }
+
+    fun listLibraries() =
+        if (libraries.isEmpty())  "\tNO BOOKS ADDED"
+        else  Utilities.formatSetString(libraries)
+
+    override fun toString(): String {
+        val archived = if (isBookArchived) 'Y' else 'N'
+        return "$bookId: $bookTitle, Rating($bookRating),ISBN($bookISBN), Genre($bookGenre), Archived($archived) \n${listLibraries()}"
+    }
+
+    fun checkBookCompletionStatus(): Boolean {
+        if (libraries.isNotEmpty()) {
+            for (library in libraries) {
+                if (!library.isLibraryComplete) {
+                    return false
+                }
+            }
+        }
+        return true //a book with empty libraries can be archived, or all libraries are complete
+    }
 }
